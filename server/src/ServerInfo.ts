7162720861597b3f31bf8e3ad4b109658dd4d91c
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-11-29 15:31:15 -0500

    Implement server info module and add server info environment json

--- server/src/ServerInfo.ts
+++ server/src/ServerInfo.ts
@@ -1,1 +1,22 @@
-export const serverInfo = {};
+import path from "path";
+import fs from "fs";
+
+export interface IServerInfo {
+	smtp: {
+		host: string;
+		port: number;
+		auth: { user: string; pass: string };
+	};
+	imap: {
+		host: string;
+		port: number;
+		auth: { user: string; pass: string };
+	};
+}
+
+const rawInfo: string = fs.readFileSync(
+	path.join(__dirname, "../serverInfo.json"),
+	"utf-8"
+);
+export let serverInfo: IServerInfo;
+serverInfo = JSON.parse(rawInfo);
