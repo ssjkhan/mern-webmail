62a52e0c104afcbcd8f3e7d6975acf3ae2038a34
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-11-29 14:28:09 -0500

    Set up main.ts for routing with tempalte middelware

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,8 +3,8 @@ import express, { Express, NextFunction, Request, Response } from "express";
 import { serverInfo } from "./ServerInfo";
 import * as IMAP from "./IMAP";
 import * as SMTP from "./SMTP";
-import * as Contacts from "./Contacts";
-import { IContact } from "./Contacts";
+import * as Contacts from "./contacts";
+import { IContact } from "./contacts";
 
 // Initialize App
 const app: Express = express();
@@ -12,7 +12,9 @@ const app: Express = express();
 // Middleware for parsing request bodies
 app.use(express.json());
 
+// static path way
 app.use("/", express.static(path.join(__dirname, "../../client/dist")));
+
 app.use(function (
 	inRequest: Request,
 	inResponse: Response,
