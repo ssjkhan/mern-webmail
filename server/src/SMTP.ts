cb34c36d7d672033159cb45e11463de866c605f1
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-11-29 15:55:49 -0500

    Implement Worker class for SMTP and sendMessage function

--- server/src/SMTP.ts
+++ server/src/SMTP.ts
@@ -1,4 +1,24 @@
+import Mail from "nodemailer/lib/mailer";
+import * as nodemailer from "nodemailer";
+import { SendMailOptions, SentMessageInfo } from "nodemailer";
+import { IServerInfo } from "./ServerInfo";
+
 export class Worker {
-	constructor(...args: any) {}
-	sendMessage(...args: any) {}
+	serverInfo: IServerInfo;
+
+	constructor(serverInfo_: IServerInfo) {
+		this.serverInfo = serverInfo_;
+	}
+	sendMessage(options: SendMailOptions): Promise<string> {
+		return new Promise((resolve, reject) => {
+			const transport: Mail = nodemailer.createTransport(this.serverInfo.smtp);
+			transport.sendMail(options, (err: Error | null, info: SentMessageInfo) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(err);
+				}
+			});
+		});
+	}
 }
