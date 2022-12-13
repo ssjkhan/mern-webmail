cfbf16dca1a9778c908db6b9184bda9e091473f1
sk <jsckhan@outlook.com>
2022-12-13 09:39:01 -0500

    implement dialog material ui components for baselayout

--- client/src/code/components/BaseLayout.tsx
+++ client/src/code/components/BaseLayout.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { createState } from "../state";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 
 class BaseLayout extends React.Component {
 	state = createState(this);
 	render() {
-		return <div className="appContainer"></div>;
+		return (
+			<div className="appContainer">
+				<Dialog
+					open={this.state.pleaseWaitVisible}
+					disableBackdropClick={true}
+					disableEscapeKeyDown={true}
+					transitionDuration={0}
+				>
+					<DialogTitle style={{ textAlign: "center" }}>Please Wait</DialogTitle>
+					<DialogContent>
+						<DialogContentText> ... Contacting Server ...</DialogContentText>
+					</DialogContent>
+				</Dialog>
+			</div>
+		);
 	}
 }
 
