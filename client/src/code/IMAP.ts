cd02941b06105ebfa6b6ea0b471588f10bad9842
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-12-13 09:16:09 -0500

    implement IMAP client functionality mirroring server

--- client/src/code/IMAP.ts
+++ client/src/code/IMAP.ts
@@ -1,9 +1,44 @@
+import { responsiveFontSizes } from "@material-ui/core";
+import axios, { AxiosResponse } from "axios";
+import config from "./config";
+
 export class Worker {
-	listMailboxes() {
-		return [];
+	async listMailboxes(): Promise<IMailbox[]> {
+		const resp: AxiosResponse = await axios.get(
+			`${config.serverAddress}/mailboxes`
+		);
+
+		return resp.data;
+	}
+
+	async listMessages(mailbox: string): Promise<IMessage[]> {
+		const resp: AxiosResponse = await axios.get(
+			`${config.serverAddress}/mailboxes/${mailbox}`
+		);
+		return resp.data;
+	}
+
+	async getMessageBody(id: string, mailbox: string): Promise<string> {
+		const resp: AxiosResponse = await axios.get(
+			`${config.serverAddress}/messages/${mailbox}/${id}`
+		);
+		return resp.data;
+	}
+
+	async deleteMessage(id: string, mailbox: string): Promise<void> {
+		await axios.delete(`${config.serverAddress}/messages/${mailbox}/${id}`);
 	}
 }
 
-export class IMailbox {}
+export interface IMailbox {
+	name: string;
+	path: string;
+}
 
-export default {};
+export interface IMessage {
+	id: string;
+	date: string;
+	from: string;
+	subject: string;
+	body?: string;
+}
