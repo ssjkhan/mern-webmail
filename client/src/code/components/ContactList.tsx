9e36a7ee05ca472fa543ae6c2cffd894850ff654
sk <jsckhan@outlook.com>
2022-12-13 10:38:08 -0500

    add templating for components: contactList, Mailbox, MessageView

---
+++ client/src/code/components/ContactList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ContactList extends React.Component {
+	render() {
+		return <p>This is ContactList Component</p>;
+	}
+}
+
+export default ContactList;
---
+++ client/src/code/components/Mailbox.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Mailbox extends React.Component {
+	render() {
+		return <p>This is Mailbox Component</p>;
+	}
+}
+
+export default Mailbox;
