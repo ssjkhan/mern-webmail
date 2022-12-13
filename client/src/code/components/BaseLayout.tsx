e0a42cf5912e6f2ce9f2011ca915e490a3f8b2ad
sk <jsckhan@outlook.com>
2022-12-13 10:43:16 -0500

    implement BaseLayout Component

--- client/src/code/components/BaseLayout.tsx
+++ client/src/code/components/BaseLayout.tsx
@@ -5,6 +5,14 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 
+import Toolbar from "./Toolbar";
+import Mailbox from "./Mailbox";
+import WelcomeView from "./WelcomeView";
+import MessageList from "./MessageList";
+import MessageView from "./MessageView";
+import ContactList from "./ContactList";
+import ContactView from "./ContactView";
+
 class BaseLayout extends React.Component {
 	state = createState(this);
 	render() {
@@ -21,6 +29,29 @@ class BaseLayout extends React.Component {
 						<DialogContentText> ... Contacting Server ...</DialogContentText>
 					</DialogContent>
 				</Dialog>
+				<div className="toolbar">{/* <Toolbar state={this.state} /> */}</div>
+				<div className="mailboxList">{/* <MailboxList state={this.state}/> */}</div>
+				<div className="centerArea">
+					<div className="messageList">
+						{/* <MessageList state={this.state}></MessageList> */}
+					</div>
+					<div className="centerViews">
+						{this.state.currentView === "welcome" && <WelcomeView />}
+						{
+							(this.state.currentView === "message" ||
+								this.state.currentView === "compose") &&
+								true // <MessageView state={this.state} />
+						}
+						{
+							(this.state.currentView === "contact" ||
+								this.state.currentView === "contatAdd") &&
+								true // <ContactView state={this.state} />
+						}
+					</div>
+					<div className="contactList">
+						{/* <ContactList state={this.state}/> */}
+					</div>
+				</div>
 			</div>
 		);
 	}
